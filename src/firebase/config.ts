6e2ab89f16bc07f2932be18b9762c02190ff0f15
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-11-14 14:39:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-525105839-159e4",
+  "appId": "1:793841793308:web:408f4035e4178bf2f962d9",
+  "apiKey": "AIzaSyBZly_kWYNRG5Kgt_uyTqDXGXa4_T3jGzk",
+  "authDomain": "studio-525105839-159e4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "793841793308"
+};
