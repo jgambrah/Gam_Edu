76c78b9adf1de9d6b12f1c26ab4d58f3676885be
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-11-17 08:44:30 +0000

    Try fixing this error: `Runtime Error: Error: ./src/app/dashboard/academ

--- src/app/dashboard/academics/page.tsx
+++ src/app/dashboard/academics/page.tsx
@@ -1,9 +1,7 @@
+'use client';
 
 import { Suspense } from 'react';
 import ClientBoundary from '../client-boundary';
-
-'use client';
-
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
