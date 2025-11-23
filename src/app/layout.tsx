8ab4d2db48af82f101d206fa48aca2248b5810bc
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-11-23 09:25:24 +0000

    the opened and disappeared shortly with same error message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 
 import type { Metadata } from 'next';
 import './globals.css';
-import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
 
 export const metadata: Metadata = {
@@ -23,7 +22,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <FirebaseClientProvider>{children}</FirebaseClientProvider>
-        <Toaster />
       </body>
     </html>
   );
