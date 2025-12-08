2ce4373ffcc11b5d26c13a5e62ef1de40e37d744
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-12-08 17:16:50 +0000

    Try fixing this error: `Console Error: Each child in a list should have

--- src/app/dashboard/academics/academics-client.tsx
+++ src/app/dashboard/academics/academics-client.tsx
@@ -306,13 +306,13 @@ function ClassDetailsDialog({ classData, teachers, students, timetable, subjects
                                 <CardContent>
                                     <Form {...form}>
                                         <form key={classData.id} onSubmit={form.handleSubmit(onUpdate)} className="space-y-4">
-                                            <FormField control={form.control} name="teacherId" render={({ field }) => (
+                                            <FormField key={`${classData.id}-teacher`} control={form.control} name="teacherId" render={({ field }) => (
                                                 <FormItem><FormLabel>Class Teacher</FormLabel><Select onValueChange={field.onChange} defaultValue={field.value} disabled={!canManage}><FormControl><SelectTrigger><SelectValue placeholder="Select a teacher" /></SelectTrigger></FormControl><SelectContent>{teachers.map(t => <SelectItem key={t.uid} value={t.uid}>{t.firstName} {t.lastName}</SelectItem>)}</SelectContent></Select></FormItem>
                                             )}/>
-                                            <FormField control={form.control} name="capacity" render={({ field }) => (
+                                            <FormField key={`${classData.id}-capacity`} control={form.control} name="capacity" render={({ field }) => (
                                                 <FormItem><FormLabel>Class Capacity</FormLabel><FormControl><Input type="number" {...field} disabled={!canManage} /></FormControl></FormItem>
                                             )}/>
-                                            <FormField control={form.control} name="description" render={({ field }) => (
+                                            <FormField key={`${classData.id}-description`} control={form.control} name="description" render={({ field }) => (
                                                 <FormItem><FormLabel>Description</FormLabel><FormControl><Textarea {...field} disabled={!canManage} /></FormControl></FormItem>
                                             )}/>
                                             {canManage && <Button type="submit" disabled={isSubmitting}>{isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin"/>}Save Changes</Button>}
