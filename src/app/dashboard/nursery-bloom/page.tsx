f27802dc0237337476a31cbe5139b2d56a254389
JAMES GAMBRAH <jamesgambrah@gmail.com>
2026-01-28 11:31:41 +0000

    PLEASE CREATE A SIDEBAR UNDER CLUBS AND ACTVITITIES AND NAME IT A NURSER

---
+++ src/app/dashboard/nursery-bloom/page.tsx
@@ -0,0 +1,19 @@
+
+'use client';
+
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+
+export default function NurseryBloomPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Nursery Bloom</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>This is the Nursery Bloom page. Content will be added here.</p>
+      </CardContent>
+    </Card>
+  );
+}
+
+    
