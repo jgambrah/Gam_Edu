a0ba12f8a11619aa7f0100116b68df439aa15c95
JAMES GAMBRAH <jamesgambrah@gmail.com>
2026-01-30 12:30:14 +0000

    DID YOU REMOVE THE NURSERY BLOOM SIDE BAR

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -254,6 +254,12 @@ export const navItems: NavItem[] = [
             icon: Gamepad2,
             roles: ['Student', 'Teacher', 'Administrator', 'Director'],
         },
+        {
+            path: '/dashboard/live-classroom',
+            title: 'Live Classroom',
+            icon: Clapperboard,
+            roles: ['Student', 'Teacher', 'Administrator', 'Director'],
+        },
     ]
   },
   {
@@ -645,4 +651,5 @@ export const MOCK_CROSSWORD_PUZZLES = [
   },
 ];
 
+    
     
