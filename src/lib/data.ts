107d09d5b876b8a700ad47e06859aad11428a95d
JAMES GAMBRAH <jamesgambrah@gmail.com>
2026-01-31 12:52:21 +0000

    currently teachers can see students and staff and can even change a staf

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -80,7 +80,7 @@ export const navItems: NavItem[] = [
     path: '/dashboard/people',
     title: 'People Management',
     icon: Users,
-    roles: ['Director', 'Administrator', 'Teacher', 'Parent'],
+    roles: ['Director', 'Administrator', 'Parent'],
     subItems: [
         {
             path: '/dashboard/admissions',
@@ -92,13 +92,13 @@ export const navItems: NavItem[] = [
             path: '/dashboard/staff-management-v2',
             title: 'Staff Management',
             icon: UserCog,
-            roles: ['Director', 'Administrator', 'Teacher'],
+            roles: ['Director', 'Administrator'],
         },
         {
             path: '/dashboard/students-v3',
             title: 'Students',
             icon: GraduationCap,
-            roles: ['Director', 'Administrator', 'Teacher'],
+            roles: ['Director', 'Administrator'],
         },
         {
             path: '/dashboard/parents-v2',
