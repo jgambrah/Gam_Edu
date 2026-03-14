2964d5f07939cb7a0c008521566a8999eb097015
JAMES GAMBRAH <jamesgambrah@gmail.com>
2026-03-14 11:02:36 +0000

    ## Error Type Console Error
    ## Error Message
    In HTML, <div> cannot be a

--- src/app/dashboard/assessments/page.tsx
+++ src/app/dashboard/assessments/page.tsx
@@ -182,7 +182,7 @@ function BehavioralLog() {
                             <TableRow key={i}>
                                 <TableCell><Skeleton className="h-4 w-24" /></TableCell>
                                 <TableCell><Skeleton className="h-4 w-32" /></TableCell>
-                                <Skeleton className="h-4 w-28" />
+                                <TableCell><Skeleton className="h-4 w-28" /></TableCell>
                                 <TableCell><Skeleton className="h-4 w-full" /></TableCell>
                             </TableRow>
                         )) : records?.length === 0 ? (
